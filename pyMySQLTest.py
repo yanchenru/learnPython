b0fa13db12707578734e91a1466eec08bd5b21f0
chenru <yanchenru@gmail.com>
2016-03-14 00:51:56 -0600

    Update pyMySQLTest.py

--- pyMySQLTest.py
+++ pyMySQLTest.py
@@ -1,1 +1,4 @@
-import pymysql
+import pymysql 
+conn = pymysql.connect(host='127.0.0.1', unix_socket='/tmp/mysql.sock', user='root', passwd=None, db='mysql') 
+cur = conn.cursor() 
+cur.execute("USE scraping")
